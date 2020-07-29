4b9a8f3a6086d32392e0580f4fcca655d4db7b5c
James Musselwhite <jmusselwhite@pivotal.io>
2020-07-29 14:28:16 -0700

    renaming to tanzulab

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-Everything you need to deploy Spring PetClinic
+Everything you need to deploy the Tanzu Lab application. Tanzu Lab is made up of Spring PetClinic and some other service I haven't decided on yet.
 
-`setup.sh` - creates the Namespace, creates a TLS secret for Ingress, creates a Registry secret for TBS, and creates the TBS Image for PetClinic
+`setup.sh` - creates the Namespace, creates a TLS secret for Ingress, creates a Registry secret for TBS, and creates the TBS Images for PetClinic and the other service
 
-`manifests` - all the K8s manifests to deploy the app
+`manifests` - all the K8s manifests to deploy Tanzu Lab
 
-`pipeline` - all the files needed to create the pipeline for PetClinic
+`pipeline` - all the files needed to create the pipeline for Tanzu Lab.
--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # Make the namespace for the PetClinic K8s resources
-kubectl create ns petclinic
+kubectl create ns tanzulab
 
 # Make the Ingress TLS pem files locally on disk
 mkcert \
@@ -13,20 +13,20 @@ petclinic.lab.home
 
 # Make the TLS secret for PetClinic Ingress using the on-disk pem files
 kubectl create secret tls petclinic-tls \
---namespace petclinic \
+--namespace tanzulab \
 --cert=./tls.crt \
 --key=./tls.key
 
 # Make registry secret for TBS
 REGISTRY_PASSWORD=Harbor12345 kp secret create harbor2-creds \
---namespace petclinic \
+--namespace tanzulab \
 --registry harbor.lab.home \
 --registry-user admin
 
 # Setup TBS to build PetClinic
 kp image create petclinic \
 harbor.lab.home/library/petclinic \
---namespace petclinic \
+--namespace tanzulab \
 --git https://github.com/techgnosis/spring-petclinic.git \
 --git-revision HEAD
 
