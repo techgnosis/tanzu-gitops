b62960fc2a047909c090051b54c07aa55a269e82
James Musselwhite <jmusselwhite@pivotal.io>
2020-07-30 15:05:38 -0700

    working

--- test-pipeline/fly.sh
+++ test-pipeline/fly.sh
@@ -8,4 +8,5 @@ fly set-pipeline -t lab \
 -p test-pipeline \
 -c pipeline.yml \
 -v role_id="${ROLE_ID}" \
--v secret_id="${SECRET_ID}"
+-v secret_id="${SECRET_ID}" \
+-v ca_cert="${CA_CERT}"
--- test-pipeline/vars.sh
+++ test-pipeline/vars.sh
@@ -1,2 +1,3 @@
+export CA_CERT=`cat "$(mkcert -CAROOT)"/rootCA.pem`
 export ROLE_ID=$(vault read -field=role_id auth/approle/role/concourse/role-id)
 export SECRET_ID=$(vault write -field=secret_id -f auth/approle/role/concourse/secret-id)
