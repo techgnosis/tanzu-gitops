3250d26d9d42d0f5dc8aafa5d7b46fa4263f3373
James Musselwhite <jmusselwhite@pivotal.io>
2020-07-31 00:22:24 -0700

    finally works

--- populate-vault.sh
+++ populate-vault.sh
@@ -2,18 +2,14 @@
 
 set -euo pipefail
 
-
+# TKGI
 vault write concourse/main/tkgicluster value=""
-
-vault write concourse/main/ca_cert value=""
-
-vault write concourse/main/concourse_github_privatekey value=""
-
 vault write concourse/main/tkgiapi value=""
-
 vault write concourse/main/tkgiuser value=""
-
 vault write concourse/main/tkgipassword value=""
 
+
+vault write concourse/main/ca_cert value=""
+vault write concourse/main/concourse_github_privatekey value=""
 
 
