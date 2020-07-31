8a174216eac884358d29d15e0b0e0f7be141c8bb
James Musselwhite <jmusselwhite@pivotal.io>
2020-07-30 18:35:31 -0700

    in progress

--- setup-spring-petclinic.sh
+++ setup-spring-petclinic.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # Make the namespace for the PetClinic K8s resources
-kubectl create ns tanzulab
+kubectl create ns spring-petclinic
 
 # Make the Ingress TLS pem files locally on disk
 mkcert \
@@ -13,20 +13,20 @@ spring-petclinic.lab.home
 
 # Make the TLS secret for PetClinic Ingress using the on-disk pem files
 kubectl create secret tls spring-petclinic-tls \
---namespace tanzulab \
+--namespace spring-petclinic \
 --cert=./tls.crt \
 --key=./tls.key
 
 # Make registry secret for TBS
 REGISTRY_PASSWORD=Harbor12345 kp secret create harbor2-creds \
---namespace tanzulab \
+--namespace spring-petclinic \
 --registry harbor.lab.home \
 --registry-user admin
 
 # Setup TBS to build PetClinic
 kp image create spring-petclinic \
 harbor.lab.home/library/spring-petclinic \
---namespace tanzulab \
+--namespace spring-petclinic \
 --git https://github.com/techgnosis/spring-petclinic.git \
 --git-revision bedb97afb61dd4422d6719c30838194841c5caec
 
