e447e79233cd64a8e28faa8264fb0e74dc8a5276
James Musselwhite <jmusselwhite@pivotal.io>
2020-07-31 07:12:29 -0700

    dev

--- setup-secrets.sh
+++ setup-secrets.sh
@@ -2,15 +2,6 @@
 
 set -euo pipefail
 
-export CONCOURSE_URL=""
-export HARBOR_URL=""
-export SPRING_PETCLINIC_URL=""
-export TKGI_URL=""
-export TKGI_USER=""
-export TKGI_PASSWORD=""
-export TKGI_CLUSTER=""
-
-
 # Concourse TLS
 mkcert \
 -cert-file tls.crt \
@@ -49,14 +40,4 @@ kubectl create secret tls spring-petclinic-tls \
 --key=./tls.key \
 --dry-run=client \
 -o json | kubeseal > manifests/spring-petclinic/ingress-tls.json
-
-# Pipeline secrets
-kubectl create secret generic tanzu-gitops \
---namespace concourse-secrets-main \
---from-literal=tkgi_cluster="${TKGI_CLUSTER}" \
---from-literal=tkgi_url="${TKGI_URL}" \
---from-literal=tkgi_user="${TKGI_USER}" \
---from-literal=tkgi_password="${TKGI_PASSWORD}" \
---dry-run=client \
--o json | kubeseal > manifests/concourse/pipeline-secrets.json
 
