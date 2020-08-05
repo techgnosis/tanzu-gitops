05bf21725e6f9b8bc12b8fb3362323db12c0935d
James Musselwhite <jmusselwhite@pivotal.io>
2020-08-05 15:17:09 -0700

    Task pointing at correct namespace that houses TBS Image resource

--- concourse/tasks/build-spring-petclinic-image/build-spring-petclinic-image.sh
+++ concourse/tasks/build-spring-petclinic-image/build-spring-petclinic-image.sh
@@ -11,5 +11,5 @@ pks get-credentials ${tkgicluster}
 
 cd spring-petclinic
 REVISION=$(git rev-parse HEAD)
-kp -n spring-petclinic image patch spring-petclinic --git-revision ${REVISION}
+kp -n images image patch spring-petclinic --git-revision ${REVISION}
 
