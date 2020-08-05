9c3f74edc080a767eb308da24d20c191cb4f1618
James Musselwhite <jmusselwhite@pivotal.io>
2020-08-05 12:56:51 -0700

    Use kapp to install helm-operator

--- helm-operator/install.sh
+++ helm-operator/install.sh
@@ -6,10 +6,13 @@ helm repo add fluxcd https://charts.fluxcd.io
 
 export VERSION="1.1.0"
 
-kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/$VERSION/deploy/crds.yaml
-
-helm upgrade -i helm-operator fluxcd/helm-operator \
+helm template helm-operator fluxcd/helm-operator \
 --namespace helm-operator \
---create-namespace \
 --set helm.versions=v3 \
---version $VERSION
+--version $VERSION | kapp deploy \
+-a helm-operator \
+--into-ns=helm-operator \
+-y \
+-f namespace.yml \
+-f https://raw.githubusercontent.com/fluxcd/helm-operator/$VERSION/deploy/crds.yaml \
+-f-
