e5397e231922ad0db7d003fdac4a2d262332eed1
James Musselwhite <jmusselwhite@pivotal.io>
2020-08-05 15:27:38 -0700

    Pushing up new secrets

--- setup-infra-secrets.sh
+++ setup-infra-secrets.sh
@@ -17,7 +17,8 @@ export CONCOURSE_GITHUB_PRIVATEKEY="$(cat $HOME/.ssh/concourse)"
 read -p "TKGI URL: " TKGI_URL
 read -p "TKGI_USER: " TKGI_USER
 read -p "TKGI_PASSWORD: " TKGI_PASSWORD
-read -p "TKGI_CLUSTER: " TKGI_CLUSTER
+read -p "INFRA_CLUSTER: " INFRA_CLUSTER
+read -p "WORKLOAD_CLUSTER: " WORKLOAD_CLUSTER
 read -p "WAVEFRONT API TOKEN: " WAVEFRONT_API_TOKEN
 read -p "WAVEFRONT_URL: " WAVEFRONT_URL
 
@@ -27,7 +28,8 @@ read -p "WAVEFRONT_URL: " WAVEFRONT_URL
 
 kubectl create secret generic tanzu-gitops \
 --namespace concourse-main \
---from-literal=tkgi_cluster="${TKGI_CLUSTER}" \
+--from-literal=infra_cluster="${INFRA_CLUSTER}" \
+--from-literal=workload_cluster="${WORKLOAD_CLUSTER}" \
 --from-literal=tkgi_url="${TKGI_URL}" \
 --from-literal=tkgi_user="${TKGI_USER}" \
 --from-literal=tkgi_password="${TKGI_PASSWORD}" \
