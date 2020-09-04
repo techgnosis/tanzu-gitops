98fd69a60808d1eb0315431e492d23a7ef2a6744
James Musselwhite <jmusselwhite@pivotal.io>
2020-09-04 01:52:43 -0700

    new tasks that don't use sd or push to git

--- concourse/tasks/deploy-spring-petclinic-image/deploy-spring-petclinic-image.sh
+++ concourse/tasks/deploy-spring-petclinic-image/deploy-spring-petclinic-image.sh
@@ -9,18 +9,6 @@ pks login -a ${tkgiapi} \
 
 pks get-credentials ${tkgicluster}
 
-git clone tanzu-gitops tanzu-gitops-modified
-
 export DIGEST=$(cat spring-petclinic-image/digest)
 
-sd harbor.lab.home/library/spring-petclinic.+ harbor.lab.home/library/spring-petclinic@$DIGEST tanzu-gitops-modified/manifests/spring-petclinic/rollout.yml
-
-# deploy it
-kubectl apply -f tanzu-gitops-modified/manifests/spring-petclinic
-
-git config --global user.email "concourse@lab.home"
-git config --global user.name "Concourse"
-
-cd tanzu-gitops-modified
-git add manifests/spring-petclinic/rollout.yml
-git commit -m "Updating rollout to use new image"
+kubectl argo rollouts set image -n spring-petclinic spring-petclinic=harbor.lab.home/library/spring-petclinic@$DIGEST
