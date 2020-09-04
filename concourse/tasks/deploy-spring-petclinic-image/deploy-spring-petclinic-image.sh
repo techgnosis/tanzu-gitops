2a63cc3964a1b606d0f619105aa4853ffeb06fc2
James Musselwhite <jmusselwhite@pivotal.io>
2020-09-04 01:56:31 -0700

    new tasks that don't use sd or push to git

--- concourse/tasks/deploy-spring-petclinic-image/deploy-spring-petclinic-image.sh
+++ concourse/tasks/deploy-spring-petclinic-image/deploy-spring-petclinic-image.sh
@@ -11,4 +11,4 @@ pks get-credentials ${tkgicluster}
 
 export DIGEST=$(cat spring-petclinic-image/digest)
 
-kubectl argo rollouts set image -n spring-petclinic spring-petclinic=harbor.lab.home/library/spring-petclinic@$DIGEST
+kubectl argo rollouts set image -n spring-petclinic spring-petclinic spring-petclinic=harbor.lab.home/library/spring-petclinic@$DIGEST
