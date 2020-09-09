cd9d91fdcd10bff8c9b1ad1c5252eb00c9858d0e
James Musselwhite <jmusselwhite@pivotal.io>
2020-09-08 23:36:26 -0700

    Adding new tasks for Harbor

---
+++ concourse/tasks/deploy-harbor/deploy-harbor.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+set -xeuo pipefail
+
+pks login -a ${tkgiapi} \
+-u ${tkgiuser} \
+-p ${tkgipassword} \
+-k
+
+pks get-credentials ${tkgicluster}
+
+kapp deploy -a harbor -f tanzu-gitops/manifests/harbor -y
