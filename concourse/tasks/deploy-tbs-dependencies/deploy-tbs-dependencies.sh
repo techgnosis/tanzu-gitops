5f1b196d74bfdf67bf2e6c07507c7607fe47f7b8
James Musselwhite <jmusselwhite@pivotal.io>
2020-09-16 17:15:58 -0700

    Getting desperate. Why does docker always try to login to 'domain'

--- concourse/tasks/deploy-tbs-dependencies/deploy-tbs-dependencies.sh
+++ concourse/tasks/deploy-tbs-dependencies/deploy-tbs-dependencies.sh
@@ -10,6 +10,7 @@ tkgi login -a ${tkgiapi} \
 tkgi get-credentials ${tkgicluster}
 
 docker login registry.pivotal.io -u ${pivnetusername} -p ${pivnetpassword}
+echo "${harbordomain}"
 docker login "${harbordomain}" -u admin -p Harbor12345
 
 kp import -f ./tbs-dependencies/descriptor-*.yaml
