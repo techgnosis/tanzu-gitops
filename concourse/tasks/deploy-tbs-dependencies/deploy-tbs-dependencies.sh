9ec3313b1c262306eb0fa2631eda03e901e5ac46
James Musselwhite <jmusselwhite@pivotal.io>
2020-09-16 17:22:03 -0700

    Fixed it but it was never about the env variable. For some reason that echo fixed the problem

--- concourse/tasks/deploy-tbs-dependencies/deploy-tbs-dependencies.sh
+++ concourse/tasks/deploy-tbs-dependencies/deploy-tbs-dependencies.sh
@@ -10,6 +10,13 @@ tkgi login -a ${tkgiapi} \
 tkgi get-credentials ${tkgicluster}
 
 docker login registry.pivotal.io -u ${pivnetusername} -p ${pivnetpassword}
+
+# Do not delete this echo statement. For some reason
+# if you do then the docker login afterward will try
+# to login to 'domain' instead of the value of harbordomain.
+# It's got to be something about how docker login processes
+# text...who knows.
+# I know you don't believe me - just try it
 echo "${harbordomain}"
 docker login "${harbordomain}" -u admin -p Harbor12345
 
