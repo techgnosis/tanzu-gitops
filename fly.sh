1edd4bdc781c6ec15ac8e457202c5d9848ff2a4b
James Musselwhite <jmusselwhite@pivotal.io>
2020-09-16 17:47:39 -0700

    no more dashes in any variables

--- fly.sh
+++ fly.sh
@@ -11,24 +11,24 @@ fly login \
 fly set-pipeline -t lab \
 -p spring-petclinic \
 -c concourse/pipeline/spring-petclinic.yml \
--v harbor-domain=$HARBOR_DOMAIN
+-v harbordomain=$HARBOR_DOMAIN
 
 fly set-pipeline -t lab \
 -p product-api \
 -c concourse/pipeline/product-api.yml \
--v harbor-domain=$HARBOR_DOMAIN
+-v harbordomain=$HARBOR_DOMAIN
 
 fly set-pipeline -t lab \
 -p harbor \
 -c concourse/pipeline/harbor.yml \
--v harbor-domain=$HARBOR_DOMAIN
+-v harbordomain=$HARBOR_DOMAIN
 
 fly set-pipeline -t lab \
 -p kubeapps \
 -c concourse/pipeline/kubeapps.yml \
--v harbor-domain=$HARBOR_DOMAIN
+-v harbordomain=$HARBOR_DOMAIN
 
 fly set-pipeline -t lab \
 -p tbs \
 -c concourse/pipeline/tbs.yml \
--v harbor-domain=$HARBOR_DOMAIN
+-v harbordomain=$HARBOR_DOMAIN
