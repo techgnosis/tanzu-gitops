60753902ab503eb7d8e3f6b8eff4945a975373d7
James Musselwhite <jmusselwhite@pivotal.io>
2020-09-24 08:23:44 -0700

    Using a single cluster called 'diy' for spring-petclinic and product-api

--- tkgi-create-clusters.sh
+++ tkgi-create-clusters.sh
@@ -1,39 +1,30 @@
 #! /usr/bin/env bash
 
-# Uses the default plans "small" and "medium"
-# Uses a custom plan called "singleVM" that has one worker node with good specs
+# Uses the default plan "small" for everything
 # Change as you see fit. These numbers work for my lab and my workloads
 
 tkgi create-cluster concourse \
 --external-hostname=k8s-concourse.$PRIMARY_DOMAIN \
---plan=singleVM
+--plan=small
 
 tkgi create-cluster harbor \
 --external-hostname=k8s-harbor.$PRIMARY_DOMAIN \
---plan=singleVM
+--plan=small
 
 tkgi create-cluster tbs \
 --external-hostname=k8s-tbs.$PRIMARY_DOMAIN \
---plan=medium \
---num-nodes=2
+--plan=small
 
-tkgi create-cluster spring-petclinic \
---external-hostname=k8s-spring-petclinic.$PRIMARY_DOMAIN \
---plan=small \
---num-nodes 1
-
-tkgi create-cluster product-api \
---external-hostname=k8s-product-api.$PRIMARY_DOMAIN \
---plan=small \
---num-nodes 1
+tkgi create-cluster diy \
+--external-hostname=k8s-diy.$PRIMARY_DOMAIN \
+--plan=small
 
 tkgi create-cluster kubeapps \
 --external-hostname=k8s-kubeapps.$PRIMARY_DOMAIN \
---plan=small \
---num-nodes 3
+--plan=small
 
 # TAS4K8s won't install on less than 5 small workers
 tkgi create-cluster tas \
 --external-hostname=k8s-tas.$PRIMARY_DOMAIN \
 --plan=small \
---num-nodes 5
+--num-nodes=5
