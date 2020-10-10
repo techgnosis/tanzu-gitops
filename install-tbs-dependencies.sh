22c1f8966e7a922349c8ef4d4859bbac57d2f8a3
James Musselwhite <jmusselwhite@pivotal.io>
2020-10-10 12:59:07 -0700

    tbs and images get their own folders

--- install-tbs-dependencies.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-
-kp import -f manifests/tbs/descriptor-100.0.19.yaml
---
+++ tbs/install-tbs-dependencies.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+kp import -f descriptor-100.0.19.yaml
