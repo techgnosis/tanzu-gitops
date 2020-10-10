8f6d261f7b868be3276521b4dd775e9b221ddd35
James Musselwhite <jmusselwhite@pivotal.io>
2020-10-10 12:52:01 -0700

    prometheus gets a folder

--- prometheus/install-prometheus.sh
+++ prometheus/install-prometheus.sh
@@ -2,9 +2,6 @@
 
 set -euo pipefail
 
-cd manifests/prometheus
-airgap.sh prometheus.yml $HARBOR_DOMAIN/library/prometheus
-
 kapp deploy -a prometheus -f <(ytt --data-values-env=YTT_PROMETHEUS \
 -f certificate.yml \
 -f clusterrole.yml \
@@ -14,4 +11,4 @@ kapp deploy -a prometheus -f <(ytt --data-values-env=YTT_PROMETHEUS \
 -f namespace.yml \
 -f sa.yml \
 -f values.yml \
--f relocated.yml)
+-f prometheus.yml)
