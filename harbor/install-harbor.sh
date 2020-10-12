158f8b14a4afa8e422c0bb4220060b9ed43ac8dc
James Musselwhite <jmusselwhite@pivotal.io>
2020-10-12 16:46:05 -0700

    removing HelmRelease

--- harbor/install-harbor.sh
+++ harbor/install-harbor.sh
@@ -3,4 +3,13 @@
 set -euo pipefail
 
 
-kapp deploy -a harbor -f <(ytt --data-values-env=YTT_HARBOR -f manifests/harbor)
+helm install harbor harbor \
+--repo https://helm.goharbor.io \
+--version 1.5.0 \
+--values helm.yml
+
+
+kapp deploy -a harbor -f <(ytt --data-values-env=YTT_HARBOR \
+-f certificate-harbor.yml \
+-f certificate-notary.yml \
+-f values.yml)
