be3b246a0fec38c036a2c12e8d09f2e41da0850c
James Musselwhite <jmusselwhite@pivotal.io>
2020-10-12 16:46:30 -0700

    removing helmrelease from minibroker

--- minibroker/install-minibroker.sh
+++ minibroker/install-minibroker.sh
@@ -2,4 +2,8 @@
 
 set -euo pipefail
 
-kapp deploy -a minibroker -f minibroker.yml -f namespace.yml
+helm install minibroker minibroker \
+--repo https://minibroker.blob.core.windows.net/charts \
+--version "0.3.1" \
+--values helm.yml \
+--wait
