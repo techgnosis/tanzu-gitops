d947ec39d307b2e69274a4d863520c06698775f7
James Musselwhite <jmusselwhite@pivotal.io>
2020-10-20 13:30:03 -0700

    Toning down mariadb so I can restore it

--- velero/create-backup.sh
+++ velero/create-backup.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+echo 'BACKUP_NUMBER=$1'
+
 BACKUP_NUMBER=$1
 
 velero backup create spring-petclinic-$BACKUP_NUMBER \
--- velero/create-restore.sh
+++ velero/create-restore.sh
@@ -2,7 +2,13 @@
 
 set -euo pipefail
 
-BACKUP_NUMBER=$1
+echo 'BACKUP_NUMBER=$1'
+echo 'RESTORE_NUMBER=$2'
 
-velero restore create spring-petclinic-restore-$BACKUP_NUMBER \
+BACKUP_NUMBER=$1
+RESTORE_NUMBER=$2
+
+
+
+velero restore create spring-petclinic-restore-$RESTORE_NUMBER \
 --from-backup spring-petclinic-$BACKUP_NUMBER
