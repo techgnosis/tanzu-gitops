3ea1003a8e36d56b7e21394b8b807c164acf6acb
James Musselwhite <jmusselwhite@pivotal.io>
2020-10-20 11:26:30 -0700

    CLI install works

--- velero/install-velero-cli.sh
+++ velero/install-velero-cli.sh
@@ -5,10 +5,11 @@ set -euo pipefail
 
 kapp deploy -a velero -f <(velero install \
 --provider aws \
---plugins velero/velero-plugin-for-aws:v1.0.0 \
+--plugins velero/velero-plugin-for-aws:v1.1.0 \
 --bucket velero \
 --secret-file ./credentials-velero \
 --use-volume-snapshots=false \
+--cacert "$(mkcert -CAROOT)/rootCA.pem" \
 --backup-location-config region=minio,s3ForcePathStyle="true",s3Url=https://minio.lab.home,insecureSkipTLSVerify="true" \
 --dry-run \
 --use-restic \
