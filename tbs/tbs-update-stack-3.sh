5772e81917717e16e474434511e25eb1ce4f8c67
James Musselwhite <jmusselwhite@pivotal.io>
2020-10-21 12:37:12 -0700

    changes

--- tbs/tbs-update-stack-3.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-
-kp clusterstack update base \
---build-image=registry.pivotal.io/tbs-dependencies/build-base:202006032114 \
---run-image=registry.pivotal.io/tbs-dependencies/run-base:202006032114
