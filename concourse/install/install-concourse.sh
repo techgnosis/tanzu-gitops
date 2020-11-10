a786162ab188550f0c994b02cb9944e7a2b8f68f
James Musselwhite <jmusselwhite@pivotal.io>
2020-11-09 22:24:30 -0800

    explicitly making all Ingress be from Contour

--- concourse/install/install-concourse.sh
+++ concourse/install/install-concourse.sh
@@ -3,21 +3,20 @@
 set -euo pipefail
 
 
-helm install concourse concourse \
+helm upgrade --install concourse concourse \
 --repo https://concourse-charts.storage.googleapis.com \
 --create-namespace \
 --namespace concourse \
 --version 11.4.0 \
 --values helm.yml \
 --set concourse.web.externalUrl="https://$YTT_CONCOURSE_concourse_hostname" \
---set web.ingress.hosts={$YTT_CONCOURSE_concourse_hostname} \
---set web.ingress.tls[0].hosts={$YTT_CONCOURSE_concourse_hostname} \
 --wait
 
 kapp deploy \
 -a concourse \
 -f <(ytt --data-values-env=YTT_CONCOURSE \
 -f certificate.yml \
+-f ingress.yml \
 -f values.yml)
 
 kapp deploy -a concourse-main -f pipeline-secrets.json
