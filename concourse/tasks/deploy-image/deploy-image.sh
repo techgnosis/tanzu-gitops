5b07a2e7748d0a10d6418c887752f9d55ac9163d
James Musselwhite <jmusselwhite@pivotal.io>
2020-11-11 00:27:51 -0800

    more specific parameters

--- concourse/tasks/deploy-image/deploy-image.sh
+++ concourse/tasks/deploy-image/deploy-image.sh
@@ -10,10 +10,6 @@ tkgi login -a ${tkgiapi} \
 tkgi get-credentials ${tkgicluster}
 
 # this whole thing only works if you name everything consistently
-namespace=${app}
-deployment=${app}
-container=${app}
-image=${app}
 
 export DIGEST=$(cat image/digest)
 kubectl -n ${namespace} set image deployment/${deployment} ${container}=${harbordomain}/library/${image}@${DIGEST}
