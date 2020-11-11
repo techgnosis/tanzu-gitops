e3c87fae536673aebfca3bd601b7e42779e8c799
James Musselwhite <jmusselwhite@pivotal.io>
2020-11-10 17:45:16 -0800

    combining wavefront into one directory

--- wavefront/install.sh
+++ wavefront/install.sh
@@ -3,6 +3,8 @@
 set -euo pipefail
 
 kapp deploy -a wavefront-proxy -f <(ytt --data-values-env=YTT_WAVEFRONT \
--f wavefront.yaml \
+-f wavefront-proxy.yaml \
 -f namespace.yml \
--f values.yml)
+-f values.yml \
+-f telegraf.yml \
+-f config.yml)
--- wavefront/telegraf/install.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-
-kapp deploy -a telegraf \
--f telegraf.yml \
--f config.yml
