adae77cc4d806e95c8a57cd263fb4906ae95709b
James Musselwhite <jmusselwhite@pivotal.io>
2020-11-11 08:13:09 -0800

    dropping the java apps

--- apps/sockshop/install.sh
+++ apps/sockshop/install.sh
@@ -4,11 +4,7 @@ set -euo pipefail
 
 kapp deploy -a sockshop -f <(ytt --data-values-env=YTT_SOCKSHOP \
 -f sockshop.yml \
--f carts.yml \
 -f front-end.yml \
--f orders.yml \
--f queue-master.yml \
--f shipping.yml \
 -f namespace.yml \
 -f certificate.yml \
 -f ingress.yml \
