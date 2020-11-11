00551e7ea077cb1901a98aeccb8f029866ebdd37
James Musselwhite <jmusselwhite@pivotal.io>
2020-11-10 18:03:32 -0800

    working great

--- apps/sockshop/load-test/locustfile.py
+++ apps/sockshop/load-test/locustfile.py
@@ -1,43 +1,34 @@
-from locust import HttpUser, task
-from random import choice
+import base64
 
+from locust import HttpUser, TaskSet, task
+from random import randint, choice
 
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+
+# Suppress only the single warning from urllib3 needed.
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+class WebTasks(TaskSet):
+
+    @task
+    def load(self):
+        base64string = base64.encodebytes(('%s:%s' % ('user', 'password')).encode()).decode().strip()
+        catalogue = self.client.get("/catalogue", verify=False).json()
+        category_item = choice(catalogue)
+        item_id = category_item["id"]
+
+        self.client.get("/", verify=False)
+        self.client.get("/login", headers={"Authorization":"Basic %s" % base64string}, verify=False)
+        self.client.get("/category.html", verify=False)
+        self.client.get("/detail.html?id={}".format(item_id), verify=False)
+        self.client.delete("/cart", verify=False)
+        self.client.post("/cart", json={"id": item_id, "quantity": 1}, verify=False)
+        self.client.get("/basket.html", verify=False)
+        self.client.post("/orders", verify=False)
 
 
 class SockShopUser(HttpUser):
+    tasks = {WebTasks}
     min_wait = 0
-    max_wait = 0        
-    
-    def on_start(self):
-        catalogue = self.client.get("/catalogue").json()
-        category_item = choice(catalogue)
-        self.item_id = category_item["id"]
-
-    @task
-    def root(self):
-        self.client.get("/")
-
-    @task
-    def get_category(self):
-        self.client.get("/category.html")
-
-    @task
-    def get_detail(self):
-        self.client.get("/detail.html?id={}".format(self.item_id))
-
-    def delete_cart(self):
-        self.client.delete("/cart")
-
-    def post_cart(self):
-        self.client.post("/cart", json={"id": self.item_id, "quantity": 1})
-
-    def get_basket(self):
-        self.client.get("/basket.html")
-
-    def post_orders(self):
-        self.client.post("/orders")
-
-
-
-
-
+    max_wait = 0
