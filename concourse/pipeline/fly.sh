20aba4ee5d2bb4193a986745de68794a4f17e9ce
James Musselwhite <jmusselwhite@pivotal.io>
2020-11-11 01:03:05 -0800

    all of sockshop is onboarded

--- concourse/pipeline/fly.sh
+++ concourse/pipeline/fly.sh
@@ -19,8 +19,28 @@ fly set-pipeline -t lab \
 -v harbordomain=$HARBOR_DOMAIN
 
 fly set-pipeline -t lab \
+-p sockshop-carts \
+-c sockshop-carts.yml \
+-v harbordomain=$HARBOR_DOMAIN
+
+fly set-pipeline -t lab \
 -p sockshop-front-end \
 -c sockshop-front-end.yml \
+-v harbordomain=$HARBOR_DOMAIN
+
+fly set-pipeline -t lab \
+-p sockshop-orders \
+-c sockshop-orders.yml \
+-v harbordomain=$HARBOR_DOMAIN
+
+fly set-pipeline -t lab \
+-p sockshop-queue-master \
+-c sockshop-queue-master.yml \
+-v harbordomain=$HARBOR_DOMAIN
+
+fly set-pipeline -t lab \
+-p sockshop-shipping \
+-c sockshop-shipping.yml \
 -v harbordomain=$HARBOR_DOMAIN
 
 fly set-pipeline -t lab \
