fa91830efbea4291ccbd4f7c3cf91c5429f87449
James Musselwhite <jmusselwhite@pivotal.io>
2020-11-11 08:06:44 -0800

    losing this comment

--- concourse/tasks/deploy-image/deploy-image.sh
+++ concourse/tasks/deploy-image/deploy-image.sh
@@ -9,7 +9,5 @@ tkgi login -a ${tkgiapi} \
 
 tkgi get-credentials ${tkgicluster}
 
-# this whole thing only works if you name everything consistently
-
 export DIGEST=$(cat image/digest)
 kubectl -n ${namespace} set image deployment/${deployment} ${container}=${harbordomain}/library/${image}@${DIGEST}
