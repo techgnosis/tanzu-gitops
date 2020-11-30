372573f2535bff705f9537a8d9eaabe20e916401
James Musselwhite <james.musselwhite@gmail.com>
2020-11-29 16:51:34 -0800

    changes

---
+++ apps/sockshop/install.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+kapp deploy -a sockshop -f <(ytt --data-values-env=YTT_SOCKSHOP \
+-f sockshop.yml \
+-f front-end.yml \
+-f namespace.yml \
+-f certificate.yml \
+-f ingress.yml \
+-f values.yml \
+-f dbs.yml)
