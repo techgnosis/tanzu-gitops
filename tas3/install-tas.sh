d4caa721db2696a9df43d5efde22440bbffcc2e5
James Musselwhite <james.musselwhite@gmail.com>
2020-12-02 13:07:15 -0800

    tas 0.6 works

--- tas3/install-tas.sh
+++ tas3/install-tas.sh
@@ -2,21 +2,19 @@
 
 set -euo pipefail
 
-echo "Installing Metrics Server"
-kubectl apply \
--f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.6/components.yaml
-
-# This script is powered by `ytt`
-# `ytt` is informed by environment variables. See `.envrc.template`
-# YTT values files are required even when using YTT_ environment variables
+# Metrics server already exists in TKGI
+#echo "Installing Metrics Server"
+#kubectl apply \
+#-f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.6/components.yaml
 
 if [ -d "tanzu-application-service" ]; then
     rm -rf tanzu-application-service
 fi
 
-tar -xvf tanzu-application-service.3.1.0-build.50.tar
+tar -xvf tanzu-application-service.0.6.0-build.1.tar
 
 cp tas3-values.yml configuration-values/values.yml
+cp apps-manager-values.yml configuration-values/
 
 ./tanzu-application-service/bin/generate-values.sh configuration-values
 ./tanzu-application-service/bin/cluster-detect.sh > configuration-values/cluster-values.yml
