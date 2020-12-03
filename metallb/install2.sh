4540eb0b2d4a44fd4bffcb3e033cc3928339aff5
James Musselwhite <james.musselwhite@gmail.com>
2020-12-02 21:54:25 -0800

    splitting metallb into two again

---
+++ metallb/install2.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+kubectl create ns metallb-system
+
+kubectl create secret generic \
+-n metallb-system memberlist \
+--from-literal=secretkey="$(openssl rand -base64 128)"
+
+kapp deploy -a metallb \
+-f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml \
+-f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml \
+-f config2.yml
