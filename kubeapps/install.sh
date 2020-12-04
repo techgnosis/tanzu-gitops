1ba1ac4a564a550067130838fece0eaa623fc0f8
James Musselwhite <james.musselwhite@gmail.com>
2020-12-04 08:25:11 -0800

    rename

--- kubeapps/install.sh
+++ kubeapps/install.sh
@@ -5,11 +5,14 @@ set -euo pipefail
 
 helm upgrade --install kubeapps kubeapps \
 --repo https://charts.trials.tac.bitnami.com/demo \
---version 4.0.2 \
+--version 5.0.0 \
 --namespace kubeapps \
 --create-namespace \
 --wait \
 --values=helm.yml
+
+# Note - the service account is for logging in to the dashboard
+# It is not something that can be created via the Helm chart
 
 kapp deploy -a kubeapps -f <(ytt --data-values-env=YTT_KUBEAPPS \
 -f certificate.yml \
