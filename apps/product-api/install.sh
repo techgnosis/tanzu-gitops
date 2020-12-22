f028520e0c873e6331770112c1ed4cebf6ee7091
James Musselwhite <james.musselwhite@gmail.com>
2020-12-22 15:17:16 -0800

    working

--- apps/product-api/install.sh
+++ apps/product-api/install.sh
@@ -3,7 +3,6 @@
 set -euo pipefail
 
 kapp deploy -a product-api -f <(ytt --data-values-env=YTT_PRODUCTAPI \
--f certificate.yml \
 -f Gateway.yml \
 -f VirtualService.yml \
 -f deployment.yml \
--- concourse/install/install.sh
+++ concourse/install/install.sh
@@ -12,11 +12,22 @@ helm upgrade --install concourse concourse \
 --set concourse.web.externalUrl="https://$YTT_CONCOURSE_concourse_hostname" \
 --wait
 
+kubectl create secret generic tanzu-gitops \
+--namespace concourse-main \
+--from-literal=tkgi_url="${TKGI_URL}" \
+--from-literal=tkgi_user="${TKGI_USER}" \
+--from-literal=tkgi_password="${TKGI_PASSWORD}" \
+--from-file=ca_cert="$(mkcert -CAROOT)/rootCA.pem" \
+--from-literal=wavefront_api_token="${WAVEFRONT_API_TOKEN}" \
+--from-literal=wavefront_url="${WAVEFRONT_URL}" \
+--from-literal=pivnet_api_token="${PIVNET_API_TOKEN}" \
+--from-literal=pivnet_username="${PIVNET_LOGIN}" \
+--from-literal=pivnet_password="${PIVNET_PASSWORD}"
+
+
 kapp deploy \
 -a concourse \
 -f <(ytt --data-values-env=YTT_CONCOURSE \
--f certificate.yml \
--f ingress.yml \
+-f gateway.yml \
+-f virtualservice.yml \
 -f values.yml)
-
-kapp deploy -a concourse-main -f pipeline-secrets.json
