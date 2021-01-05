b8b7b14a57cf2a069fc15fc856bb8590f409c56d
James Musselwhite <james.musselwhite@gmail.com>
2021-01-05 12:59:06 -0800

    More Istio work

--- grafana/configure-grafana.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-
-kubectl get secret -n grafana grafana-admin -o jsonpath='{.data.GF_SECURITY_ADMIN_PASSWORD}' -o go-template='{{.data.GF_SECURITY_ADMIN_PASSWORD | base64decode}}' && echo
--- virtualcluster/setup.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-
-chmod 600 kubeconfig.yml
-kubectl exec virtualcluster-0 --namespace virtualcluster -c syncer -- cat /root/.kube/config > kubeconfig.yml
-chmod 400 kubeconfig.yml
-
-kubectl port-forward virtualcluster-0 -n virtualcluster 8443:8443
