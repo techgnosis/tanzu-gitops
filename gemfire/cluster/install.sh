85d8aa29c6d668ec0699b500327df95f06803dcd
James Musselwhite <james.musselwhite@gmail.com>
2021-01-16 11:31:21 -0800

    important comment

--- gemfire/cluster/install.sh
+++ gemfire/cluster/install.sh
@@ -6,4 +6,7 @@ kapp deploy -a gemfire-cluster \
 -f cluster.yml \
 -f namespace.yml
 
+# kapp modifies the selector of Services it deploys
+# if you don't deploy the Pods with the Services it does not work
+# gotta use kubectl for the Service since the operator deployed the Pods
 kubectl apply -f service-redis.yml
