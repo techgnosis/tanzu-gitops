5b58b5fa7c4effffcbc63d9a3bebbf8bb421fe5e
James Musselwhite <james.musselwhite@gmail.com>
2021-01-26 16:18:33 -0800

    working

--- tmc/tmc-attach-cluster.sh
+++ tmc/tmc-attach-cluster.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 CLUSTER_NAME=$1
 
-tmc cluster attach --cluster-group $TMC_CLUSTER_GROUP_NAME --name $TMC_CLUSTER_GROUP_NAME-$CLUSTER_NAME
+tmc cluster attach \
+--cluster-group $TMC_CLUSTER_GROUP_NAME \
+--name $TMC_CLUSTER_GROUP_NAME-$CLUSTER_NAME \
+--http-proxy-url "http://192.168.1.52:8888"
+
 kapp deploy -a tmc -f k8s-attach-manifest.yaml
-rm k8s-attach-manifest.yaml
