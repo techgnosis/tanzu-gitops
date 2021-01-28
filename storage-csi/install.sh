9339f696211ece27337767979d5f946703556ada
James Musselwhite <james.musselwhite@gmail.com>
2021-01-27 18:32:05 -0800

    good

--- storage-csi/install.sh
+++ storage-csi/install.sh
@@ -4,11 +4,12 @@ set -euo pipefail
 
 CLUSTER_NAME=$1
 
-cp csi-vsphere.conf csi-vsphere-sed.conf
-sd "<CLUSTERNAME>" $CLUSTER_NAME csi-vsphere-sed.conf
-sd "<VCENTER_USER>" $VCENTER_USER csi-vsphere-sed.conf
-sd "<VCENTER_PASSWORD>" $VCENTER_PASSWORD csi-vsphere-sed.conf
-sd "<VCENTER_DATACENTER>" $VCENTER_DATACENTER csi-vsphere-sed.conf
+cp csi-vsphere-template.conf csi-vsphere.conf
+sd "<CLUSTERNAME>" $CLUSTER_NAME csi-vsphere.conf
+sd "<VCENTER_USER>" $VCENTER_USER csi-vsphere.conf
+sd "<VCENTER_PASSWORD>" $VCENTER_PASSWORD csi-vsphere.conf
+sd "<VCENTER_DATACENTER>" $VCENTER_DATACENTER csi-vsphere.conf
+sd "<VCENTER_IP>" $VCENTER_IP csi-vsphere.conf
 
 kapp deploy -a cns-csi-driver \
 -f <(kubectl create secret generic vsphere-config-secret \
