64ea554e1cd029b1061f297d1c8791f7cd508def
James Musselwhite <james.musselwhite@gmail.com>
2021-01-27 18:25:49 -0800

    no passwords in csi install now

--- storage-csi/install.sh
+++ storage-csi/install.sh
@@ -2,7 +2,13 @@
 
 set -euo pipefail
 
+CLUSTER_NAME=$1
 
+cp csi-vsphere.conf csi-vsphere-sed.conf
+sd "<CLUSTERNAME>" $CLUSTER_NAME csi-vsphere-sed.conf
+sd "<VCENTER_USER>" $VCENTER_USER csi-vsphere-sed.conf
+sd "<VCENTER_PASSWORD>" $VCENTER_PASSWORD csi-vsphere-sed.conf
+sd "<VCENTER_DATACENTER>" $VCENTER_DATACENTER csi-vsphere-sed.conf
 
 kapp deploy -a cns-csi-driver \
 -f <(kubectl create secret generic vsphere-config-secret \
