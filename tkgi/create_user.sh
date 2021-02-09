51eae6fe08cbc2c61ca4e8ae9c3c22ded1598a88
James Musselwhite <james.musselwhite@gmail.com>
2021-02-09 12:11:15 -0800

    prometheus is back, just for pods

--- tkgi/create_user.sh
+++ tkgi/create_user.sh
@@ -6,7 +6,7 @@ uaa-cli target https://$TKGI_HOSTNAME:8443
 uaa-cli get-client-credentials-token admin -s $TKGI_ADMIN_CLIENT_SECRET
 
 uaa-cli create-user tanzu-gitops \
---email tanzu-gitops@$PRIMARY_DOMAIN \
+--email tanzu-gitops@notreal.com \
 --password tanzu-gitops
 
 uaa-cli add-member pks.clusters.manage tanzu-gitops
