5b5b29d87b5b96ba760d4410d8dd675f4e8f41f9
James Musselwhite <james.musselwhite@gmail.com>
2021-03-02 17:17:28 -0800

    stuff

--- demo-app/deploy/install.sh
+++ demo-app/deploy/install.sh
@@ -2,12 +2,13 @@
 
 set -euo pipefail 
 
+DBTYPE=$1
 
 
 
 kapp deploy -a demo-app \
 -f <(ytt --data-values-env=YTT_HOMELAB \
--f deployment.yml \
+-f deployment-$DBTYPE.yml \
 -f services.yml \
 -f virtualservice.yml \
 -f values.yaml)
