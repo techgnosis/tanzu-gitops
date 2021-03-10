ad7a018af362a79d8ca567cf2b9c0359654ab151
James Musselwhite <james.musselwhite@gmail.com>
2021-03-10 08:32:09 -0800

    figured out proxy cache + charts-syncer

--- concourse/tasks/charts-syncer/charts-syncer.sh
+++ concourse/tasks/charts-syncer/charts-syncer.sh
@@ -5,6 +5,3 @@ set -xeuo pipefail
 DATE="2021-01-01"
 
 charts-syncer sync --config tanzu-gitops/tac/tac.yaml --from-date $DATE
-charts-syncer sync --config tanzu-gitops/tac/bitnami.yaml --from-date $DATE
-charts-syncer sync --config tanzu-gitops/tac/concourse.yaml --from-date $DATE
-charts-syncer sync --config tanzu-gitops/tac/jetstack.yaml --from-date $DATE
--- tac/sync.sh
+++ tac/sync.sh
@@ -1,7 +1,4 @@
 DATE="2021-01-01"
 
 charts-syncer sync --config tac.yaml --from-date $DATE
-charts-syncer sync --config bitnami.yaml --from-date $DATE
-charts-syncer sync --config concourse.yaml --from-date $DATE
-charts-syncer sync --config jetstack.yaml --from-date $DATE
 
