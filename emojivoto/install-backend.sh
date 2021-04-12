a55ff18f985798f80a98b4a713ff329baed595db
James Musselwhite <james.musselwhite@gmail.com>
2021-04-12 13:05:42 -0700

    dropping emojivoto

--- emojivoto/install-backend.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-
-kapp deploy -a emojivoto-backend \
--f workaround.yml \
--f emojivoto/kustomize/deployment/emoji.yml \
--f emojivoto/kustomize/deployment/voting.yml
--- emojivoto/install-frontend.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-
-kapp deploy -a emojivoto-frontend \
--f emojivoto/kustomize/deployment/vote-bot.yml \
--f emojivoto/kustomize/deployment/web.yml
