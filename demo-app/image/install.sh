818cad1499fa02e782da190bcd9236de49d277a5
James Musselwhite <james.musselwhite@gmail.com>
2021-04-13 15:45:00 -0700

    doing secrets correctly

--- demo-app/image/install.sh
+++ demo-app/image/install.sh
@@ -4,8 +4,12 @@ set -euo pipefail
 
 kapp deploy -a demo-app-image \
 -n demo-app \
+-f <(REGISTRY_PASSWORD=Harbor12345 kp secret create harbor-creds \
+--registry harbor.lab.home \
+--registry-user admin \
+--dry-run \
+--output yaml \
+-n demo-app) \
 -f <(ytt --data-values-env YTT_HOMELAB \
 -f image.yml \
--f tbs-service-account.yml \
--f harbor-docker-creds.yml \
 -f values.yaml)
