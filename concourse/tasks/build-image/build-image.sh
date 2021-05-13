f98dc34e65a11d5ea2fabba0db8193c9ff6c5fba
James Musselwhite <james.musselwhite@gmail.com>
2021-05-13 15:17:37 -0700

    tasks using vsphere 7 instead of tkgi

--- concourse/tasks/build-image/build-image.sh
+++ concourse/tasks/build-image/build-image.sh
@@ -2,12 +2,20 @@
 
 set -xeuo pipefail
 
-tkgi login -a ${tkgiapi} \
--u ${tkgiuser} \
--p ${tkgipassword} \
--k
+#tkgi login -a ${tkgiapi} \
+#-u ${tkgiuser} \
+#-p ${tkgipassword} \
+#-k
 
-PKS_USER_PASSWORD=${tkgipassword} tkgi get-credentials ${tkgicluster}
+#PKS_USER_PASSWORD=${tkgipassword} tkgi get-credentials ${tkgicluster}
+
+KUBECTL_VSPHERE_PASSWORD=Tkgs-admin1! \
+kubectl vsphere login \
+--server=10.0.3.2 \
+--vsphere-username tkgs-admin@vsphere.local \
+--tanzu-kubernetes-cluster-name jmusselwhite-shared-services \
+--tanzu-kubernetes-cluster-namespace myfriendthenamespace \
+--insecure-skip-tls-verify
 
 cd source-code
 REVISION=$(git rev-parse HEAD)
